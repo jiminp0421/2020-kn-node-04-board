1be5df7cd53eaddd7423a5ee02280989dcb2e574
jiminp0421 <jm00476@naver.com>
2020-11-12 22:09:46 +0900

    crud 완성

--- routes/board.js
+++ routes/board.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const moment = require('moment');
 const path = require('path');
+const fs = require('fs-extra');
 const createError = require('http-errors');
 const router = express.Router();
 const { pool } = require('../modules/mysql-conn');
-const { alert } = require('../modules/util');
+const { alert, uploadFolder } = require('../modules/util');
 const { upload, imgExt } = require('../modules/multer-conn');
 const { connect } = require('http2');
 
@@ -120,17 +121,65 @@ router.get('/update/:id', async (req, res, next) => {
 	}
 });
 
-router.post('/saveUpdate', async (req, res, next) => {
-	let connect, rs, sql, values, pug;
+router.post('/saveUpdate', upload.single('upfile'), async (req, res, next) => {
+	let connect, rs, sql, sqlRoot, values, pug;
 	let { id, title, writer, content } = req.body;
 	try {
-		sql = "UPDATE board SET title=?, writer=?, content=? WHERE id=?";
-		values = [title, writer, content, id];
+		sqlRoot = 'UPDATE board SET title=?, writer=?, content=?';
+		values = [title, writer, content];
+		if(req.allowUpload) {
+			if(req.allowUpload.allow) {
+				sql = 'SELECT savefile FROM board WHERE id='+id;
+				connect = await pool.getConnection();
+				rs = await connect.query(sql);
+				if(rs[0][0].savefile) fs.removeSync(uploadFolder(rs[0][0].savefile));
+				sqlRoot += ', savefile=?, realfile=?';
+				values.push(req.file.filename);
+				values.push(req.file.originalname);
+			}
+			else {
+				res.send(alert(`${req.allowUpload.ext}은(는) 업로드 할 수 없습니다.`, '/board'));
+			}
+		}
+		sqlRoot += ' WHERE id='+id;
 		connect = await pool.getConnection();
-		rs = await connect.query(sql, values);
+		rs = await connect.query(sqlRoot, values);
 		connect.release();
 		if(rs[0].affectedRows == 1) res.send(alert('수정되었습니다', '/board'));
 		else res.send(alert('수정에 실패하였습니다.', '/board'));
+	}
+	catch(e) {
+		if(connect) connect.release();
+		next(createError(500, e.sqlMessage));
+	}
+});
+
+router.get('/download', (req, res, next) => {
+	let { file: saveFile, name: realFile } = req.query;
+	res.download(uploadFolder(saveFile), realFile);
+});
+
+router.get('/fileRemove/:id', async (req, res, next) => {
+	let connect, rs, sql, values, list, pug;
+	try {
+		sql = 'SELECT * FROM board WHERE id='+req.params.id;
+		connect = await pool.getConnection();
+		rs = await connect.query(sql);
+		connect.release();
+		list = rs[0][0];
+		if(list.savefile) {
+			try {
+				fs.removeSync(uploadFolder(list.savefile));
+				sql = 'UPDATE board SET savefile=NULL, realfile=NULL';
+				connect = await pool.getConnection();
+				rs = await connect.query(sql);
+				connect.release();
+				res.json({code: 200});
+			}
+			catch(e) {
+				res.json({code: 500, err: e});
+			}
+		}
 	}
 	catch(e) {
 		if(connect) connect.release();
