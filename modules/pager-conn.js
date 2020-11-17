36790ac4463fe199464548c4b1155ae163b1eb06
jiminp0421 <jm00476@naver.com>
2020-11-17 21:25:59 +0900

    페이징

--- modules/pager-conn.js
+++ modules/pager-conn.js
@@ -1,16 +1,17 @@
 // let pagers = pager(2, 90, {listCnt: 5, pagerCnt: 5})
-// let pagers = pager(2, 90);
+// let pagers = pager(2, 90)
+
 const pager = (page, totalRecord, obj) => {
 	page = Number(page);
-	totalPage = Number(totalPage);
-	let { listCnt=5, pageCnt=3} = obj || {};
-	let totlaPage = Math.ceil(tatalRecord / listCnt);
-	let startIdx = (page-1)*listCnt;
-	let startPage = Math.floor((page-1) /pagerCnt) * pagerCnt+1;
-	let endPage = startPage + pagerCnt -1 > totalPage ? totalPage : startPage + pagerCnt -1;
-	let nextPage = page + 1 > totalPage ? totalPage : page + 1;
+	totalRecord = Number(totalRecord);
+	let { listCnt=5, pagerCnt=3 } = obj || {};
+	let totalPage = Math.ceil(totalRecord / listCnt);
+	let startIdx = (page - 1) * listCnt;
+	let startPage = Math.floor((page - 1) / pagerCnt) * pagerCnt + 1;
+	let endPage = startPage + pagerCnt - 1 > totalPage ? totalPage : startPage + pagerCnt - 1;
+	let nextPage = page + 1 > totalPage ? 0 : page + 1;
 	let prevPage = page - 1;
-	return { page, totalRecord, listCnt, pagerCnt, totalPage, startIdx, startPage, endPage, nextPage, prevPage};
+	return { page, totalRecord, listCnt, pagerCnt, totalPage, startIdx, startPage, endPage, nextPage, prevPage };
 }
 
 module.exports = pager;
