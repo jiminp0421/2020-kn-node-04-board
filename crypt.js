13f7967c91fb8815af7fb6bdba09accece402985
jiminp0421 <jm00476@naver.com>
2020-11-19 20:22:34 +0900

    회원가입

--- crypt.js
+++ crypt.js
@@ -1,23 +1,36 @@
-//crypt 
-//cipher
-//session
-//cookie
-//CORS (Cross Origin Resource Shart)
-//proxy - forward proxy 
-//proxy - reverse proxy
+// crypt: 단방향 암호화 
+// cipher: 양방향 암복호화
+// session: 서버가 가지는 전역변수
+// cookie: 클라이언트가 가지는 전역변수
+// CORS (Cross Origin Resource Share) - 통신규칙
+// proxy - forward proxy
+// proxy - reverse proxy
 
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+
 let password = 'abcd1234';
-let salt = 'sdfgkjdfljgef1234||sdf';
+let salt = 'axcjk9234kASFC++1234||sdf';
 let hash = crypto.createHash('sha512').update(password+salt).digest('base64');
+hash = crypto.createHash('sha512').update(hash).digest('base64');
+hash = crypto.createHash('sha512').update(hash).digest('base64');
+hash = crypto.createHash('sha512').update(hash).digest('base64');
 console.log(hash);
 
-const cipher = crypto.createCipher('aes-256-cbc', salt);
+let cipher = crypto.createCipher('aes-256-cbc', salt);
 let result = cipher.update('아버지를 아버지라...', 'utf-8', 'base64');
 result += cipher.final('base64');
 console.log(result);
 
-let dicipher = crypto.createDecipher('aes-256-cbc', salt);
-let result2 = dicipher.update(result, 'base64', 'utf-8');
-result2 += dicipher.final('utf-8');
+let decipher = crypto.createDecipher('aes-256-cbc', salt);
+let result2 = decipher.update(result, 'base64', 'utf-8');
+result2 += decipher.final('utf-8');
 console.log(result2);
+
+async function bcryptTest(){
+let bcryptHash = await bcrypt.hash(password+salt, 9);
+let bcryptCompare = await bcrypt.compare(password+salt, bcryptHash);
+	console.log(bcryptHash, bcryptCompare);
+}
+
+bcryptTest();
