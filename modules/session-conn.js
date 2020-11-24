80b5997cb459e5e128429a8f2f33add43722cbed
jiminp0421 <jm00476@naver.com>
2020-11-24 20:32:26 +0900

    session

---
+++ modules/session-conn.js
@@ -0,0 +1,15 @@
+const MySQLStore = require('express-mysql-session');
+
+module.exports = session({
+	secret: process.env.SESSION_SALT,
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: false },
+	store: new MySQLStore({
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_DATABASE
+	})
+});
